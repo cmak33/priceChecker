aafe925b40d31f07201825219d49ca5d094765ea
hahahalol9 <cmak7543@mail.ru>
2022-08-08 19:46:24 +0300

    chore: add http requests executor

---
+++ src/main/java/com/example/pricechecker/logic/httpRequests/HttpRequestsExecutor.java
@@ -0,0 +1,44 @@
+package com.example.pricechecker.logic.httpRequests;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandler;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+public class HttpRequestsExecutor {
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestsExecutor.class);
+
+    private interface SendMethod<R,T>{
+        R send(HttpClient httpClient,HttpRequest httpRequest,BodyHandler<T> bodyHandler) throws InterruptedException,IOException;
+    }
+
+    private <T> Optional<HttpResponse<T>> executeRequestSync(URI uri,BodyHandler<T> bodyHandler){
+        SendMethod<HttpResponse<T>,T> sendMethod = HttpClient::send;
+        return executeRequest(uri,sendMethod,bodyHandler);
+    }
+
+    private <T> Optional<CompletableFuture<HttpResponse<T>>> executeRequestAsync(URI uri,BodyHandler<T> bodyHandler){
+        SendMethod<CompletableFuture<HttpResponse<T>>,T> sendMethod = HttpClient::sendAsync;
+        return executeRequest(uri,sendMethod,bodyHandler);
+    }
+
+    private <R,T> Optional<R> executeRequest(URI uri,SendMethod<R,T> sendMethod,BodyHandler<T> bodyHandler){
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest httpRequest = HttpRequest.newBuilder(uri).GET().build();
+        Optional<R> result;
+        try {
+            result = Optional.of(sendMethod.send(httpClient,httpRequest,bodyHandler));
+        } catch (InterruptedException | IOException exception){
+            result = Optional.empty();
+            logger.warn("exception sending http request",exception);
+        }
+        return result;
+    }
+}
