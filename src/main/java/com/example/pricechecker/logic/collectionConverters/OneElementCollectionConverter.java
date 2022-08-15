78f0612d2617049d029f61c54d86bee8b757991b
hahahalol9 <cmak7543@mail.ru>
2022-08-15 21:36:20 +0300

    chore: update one element collection converter

--- src/main/java/com/example/pricechecker/logic/collectionConverters/OneElementCollectionConverter.java
+++ src/main/java/com/example/pricechecker/logic/collectionConverters/OneElementCollectionConverter.java
@@ -6,6 +6,13 @@ import java.util.Optional;
 public class OneElementCollectionConverter<T> implements CollectionConverter<T,T> {
     @Override
     public Optional<T> convert(List<T> collection) {
-        return collection.isEmpty()?Optional.empty():Optional.of(collection.get(0));
+        Optional<T> result;
+        if(collection.isEmpty()){
+            result = Optional.empty();
+        } else{
+            T firstElement = collection.get(0);
+            result = Optional.of(firstElement);
+        }
+        return result;
     }
 }
